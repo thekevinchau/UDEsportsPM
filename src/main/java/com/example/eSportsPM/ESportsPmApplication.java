d4fd8ce255a831f2c96d2df87603646b9898d6f7
Kevin Chau <Kevin Chau@DESKTOP-TA19N7F>
2025-04-16 20:09:12 -0400

    initial commit

---
+++ src/main/java/com/example/eSportsPM/ESportsPmApplication.java
@@ -0,0 +1,13 @@
+package com.example.eSportsPM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESportsPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESportsPmApplication.class, args);
+	}
+
+}
