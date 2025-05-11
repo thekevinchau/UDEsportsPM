e17a7cfb5eccd9d360f25a37f9a9fb5489f895cb
KEVIN CHAU <kevinch@udel.edu>
2025-05-11 17:15:13 -0400

    Moved profile retrieval into UserProfileController

--- src/main/java/com/example/eSportsPM/controllers/UserProfileController.java
+++ src/main/java/com/example/eSportsPM/controllers/UserProfileController.java
@@ -3,7 +3,7 @@ package com.example.eSportsPM.controllers;
 
 import com.example.eSportsPM.DTOs.UserProfile.PublicProfileEdit;
 import com.example.eSportsPM.DTOs.UserProfile.UserPublicProfileDTO;
-import com.example.eSportsPM.models.User;
+import com.example.eSportsPM.services.AwsServices.S3Service;
 import com.example.eSportsPM.services.UserProfileService.EditPublicProfileService;
 import com.example.eSportsPM.services.UserProfileService.GetPublicProfileService;
 import lombok.AllArgsConstructor;
@@ -20,6 +20,7 @@ public class UserProfileController {
 
     private final EditPublicProfileService editPublicProfileService;
     private final GetPublicProfileService getProfileService;
+    private final S3Service S3Service;
 
     @GetMapping("/public/{id}")
     public ResponseEntity<UserPublicProfileDTO> getPublicProfileById(@PathVariable UUID id){
@@ -30,6 +31,18 @@ public class UserProfileController {
     @GetMapping("/public")
     public ResponseEntity<UserPublicProfileDTO> getPublicProfileByUsername(@RequestParam String username){
         return getProfileService.getPublicProfileByUsername(username);
+    }
+
+    @GetMapping("/getProfileImage")
+    public ResponseEntity<String> getImagePresignedURL(@RequestParam String path){
+        return S3Service.generatePreSignedUrlDownload(path);
+    }
+
+    //client sends a photo to our backend, then the backend gets a pre-signed URL from s3 and then the server is able to return that URL back to
+    //the client so that they can make a PUT request to that URL and upload the image.
+    @GetMapping("/uploadImage")
+    public ResponseEntity<String> uploadImagePresignedURL(@RequestParam String path){
+        return S3Service.generatePreSignedUrlUpload(path);
     }
 
 
